e7846c9d6cb0b7612b66e891fc7bfb11d25f3b14
cr-zhichen <57337795+cr-zhichen@users.noreply.github.com>
2023-11-30 05:54:39 +0000

    增加跨域允许

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,13 @@ import {NsfwController} from 'app/controllers/NsfwController';
 
 const app = express();
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*'); // 允许任何源
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  next();
+});
+
 app.use(bodyParser.json());
 
 new ControllersLoader({
